16e6b131a589e84bbc87c0569a62dca2aa98282f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-09-30 15:00:55 -0400

    exported all svg icons

---
+++ src/icons/index.ts
@@ -0,0 +1,11 @@
+import CloseIcon from './CloseIcon'
+import FrontendIcon from './FrontendIcon'
+import HamburgerIcon from './HamburgerIcon'
+
+
+
+export {
+    CloseIcon as Close,
+    HamburgerIcon as HamburgerBar,
+    FrontendIcon as Frontend
+}
